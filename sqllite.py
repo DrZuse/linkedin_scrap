ba3d0affec744df18d058c5274f3c16f415f1e9d
DrZuse <53859830+DrZuse@users.noreply.github.com>
2023-09-10 13:28:36 +0300

    sqlite

--- sqllite.py
+++ sqllite.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 
 '''
 jobs.db
@@ -10,9 +10,30 @@ table tree:
 --language
 --description
 --easy apply
---nseniority_level
+--seniority_level
 
 '''
+db_name = 'jobs.db'
+if os.path.isfile(db_name):
+    db = sqlite3.connect(db_name)
+    #cursor = db.cursor()
+    rows = db.execute("SELECT * FROM jobs").fetchall()
+    print(rows)
 
-connection = sqlite3.connect("a2quarium.db")
-print(connection.total_changes)
+else: 
+    db = sqlite3.connect(db_name)
+    #cursor = db.cursor()
+    db.execute('''
+                   CREATE TABLE jobs (
+                   jobID INTEGER PRIMARY KEY NOT NULL, 
+                   title TEXT, 
+                   language TEXT, 
+                   description TEXT, 
+                   easy_apply BOOLEAN DEFAULT 0, 
+                   seniority TEXT)
+                   ''')
+    db.execute("INSERT INTO jobs VALUES (123, 'title', 'lng', 'desk', 1, 'intern')")
+    db.commit()
+    print('added')
+
+db.close()
